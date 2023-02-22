f657a2071d6411365c2164851a392da0a39323c0
abdul-09 <abdul.9.hussein@gmail.com>
2023-02-21 19:18:21 -0800

    x3 or x5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the sum all the multiples of 3 or 5 until 1024
+ * main - prints the sum of all the multiples of 3 or 5 until 1024
  * Return: Always 0 (Success)
  */
 
