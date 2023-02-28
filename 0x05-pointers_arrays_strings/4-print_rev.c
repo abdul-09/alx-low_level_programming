a5debc9cfeba79ec370be6a779711a1216e8e524
abdul-09 <abdul.9.hussein@gmail.com>
2023-02-28 01:27:03 -0800

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,19 @@
 
 void print_rev(char *s)
 {
-	while (*s == '\0')
-		_putchar(*s--);
+	int i;
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
 	_putchar('\n');
 }
