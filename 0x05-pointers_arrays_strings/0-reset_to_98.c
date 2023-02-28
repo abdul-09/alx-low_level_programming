83faf8b27418dd5d39bcb5931f90078eeb8fb850
abdul-09 <abdul.9.hussein@gmail.com>
2023-02-27 23:35:49 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the integer to 98
+ * @n: the integer to reset
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	if (&n != '98')
+	{
+		_putchar('9');
+		_putchar('8');
+	}
+	_putchar('\n');
+}
