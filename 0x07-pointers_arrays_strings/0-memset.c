cae11bec47b61da8ba1639a0be57c7d656459784
abdul-09 <abdul.9.hussein@gmail.com>
2023-03-06 06:09:20 -0800

    mem set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * _memset -  fills memory with a constant byte.
- * @s: starting address
+ * @s: starting address of memory to be filled
  * @b: the value to be filled in
- * @n: number of bytes
+ * @n: number of bytes to be changed
  * Return: changed array with new values for n bytes
  */
 
@@ -12,10 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (n > 0; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
 	}
-	return(s);
+	return (s);
 }
