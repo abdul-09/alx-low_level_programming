d6ada80d9dbba0ab3f3c6e2e4398794171e6bb64
abdul-09 <abdul.9.hussein@gmail.com>
2023-03-22 18:40:45 -0700

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,8 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= NULL || cmp == NULL)
-		return;
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return(-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
