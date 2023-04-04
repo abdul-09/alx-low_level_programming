d342e2419728cfc8fcbb3eb5fee4b029f468e17c
abdul-09 <abdul.9.hussein@gmail.com>
2023-04-04 07:24:08 -0700

    frees linked list safely

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -17,11 +17,11 @@ size_t free_listint_safe(listint_t **h)
 		return (0);
 	while (*h)
 	{
-		diff = *h - (*h)->link;
+		diff = *h - (*h)->next;
 
 		if (diff > 0)
 		{
-			temp = (*h)->link;
+			temp = (*h)->next;
 			free(*h);
 			*h = temp;
 			len++;
