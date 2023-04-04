c637cc6bfc54f8573cd3ddafcf3798add6004a22
abdul-09 <abdul.9.hussein@gmail.com>
2023-04-04 07:16:37 -0700

    get index of a certain node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (temp && i < index)
 	{
-		temp = temp->link;
+		temp = temp->next;
 		i++;
 	}
 	return (temp ? temp : NULL);
