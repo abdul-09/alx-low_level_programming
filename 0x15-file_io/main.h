753f2afc95c6d77be262f9b478e9b4f561bdd8ad
abdul-09 <abdul.9.hussein@gmail.com>
2023-04-10 22:18:48 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void close_elf(int elf);
 #endif
