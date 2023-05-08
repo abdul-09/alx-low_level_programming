ea7ad548ea611f947109838172ddb3986a63296f
abdul-09 <abdul.9.hussein@gmail.com>
2023-05-08 12:29:04 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,11 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_version(unsigned char *elf_i);
+void print_abi(unsigned char *elf_i);
+void print_osabi(unsigned char *elf_i);
+void print_type(unsigned int e_type, unsigned char *elf_i);
+void print_entry(unsigned long int e_entry, unsigned char *elf_i);
+void close_elf(int elf);
 
 #endif
