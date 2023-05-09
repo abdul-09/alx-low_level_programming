955be0d28349221b62770869f1858bc630cd40a3
abdul-09 <abdul.9.hussein@gmail.com>
2023-05-09 09:31:10 -0700

    updated header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,19 +2,20 @@
 #define MAIN_H
 
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <fcntl.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_version(unsigned char *elf_i);
-void print_abi(unsigned char *elf_i);
-void print_osabi(unsigned char *elf_i);
-void print_type(unsigned int e_type, unsigned char *elf_i);
-void print_entry(unsigned long int e_entry, unsigned char *elf_i);
+void display_version(unsigned char *e_ident);
+void display_abi(unsigned char *e_ident);
+void display_osabi(unsigned char *e_ident);
+void display_type(unsigned int e_type, unsigned char *e_ident);
+void display_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 #endif
