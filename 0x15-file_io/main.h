461204ab7ddda40a09fb079dc6f52c4e013b3cde
abdul-09 <abdul.9.hussein@gmail.com>
2023-05-10 05:40:01 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,23 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <elf.h>
 
-
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void display_version(unsigned char *e_ident);
-void display_abi(unsigned char *e_ident);
-void display_osabi(unsigned char *e_ident);
-void display_type(unsigned int e_type, unsigned char *e_ident);
-void display_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
 
 #endif
